2231c7408d9a3c7dc64e4341ccb639055640d9e5
Manon <manon.bonnet@hetic.net>
2021-05-06 10:25:36 +0200

    [js] tiens 2

--- auth/server.js
+++ auth/server.js
@@ -5,10 +5,33 @@ app.use(express.urlencoded());
 
 app.post("/auth", function (req, res) {
   /* This server is only available to nginx */
-  const streamkey = req.body.key;
+  const streamkeys = req.body.key;
+  // var userexist = false;
+
+  // const apiup = "http://localhost:3000/users/" + streamkeys;
+  // fetch(apiup, {
+  //   method: 'POST',
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   body: JSON.stringify({
+  //     streamkey: streamkeys
+  //   })
+    
+  // }).then(res => res.json()).then(res => {
+  //   if(res.username){
+  //     userexist = true;
+  //   }
+  // });
+
 
   /* You can make a database of users instead :) */
-  if (streamkey === "supersecret") {
+  // if (userexist === "true") {
+  //   res.status(200).send();
+  //   return;
+  // }
+
+  if (streamkeys === "supersecret") {
     res.status(200).send();
     return;
   }
